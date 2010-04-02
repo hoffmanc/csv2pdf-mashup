58804eb3f52c98b744f778ad63c996c98fa6bb92
Chris Hoffman <chris@linux-bacb.site>
2010-04-01 21:59:05 -0400

    successfully collected sums: now must figure out how to get counts

--- makecharts.py
+++ makecharts.py
@@ -15,17 +15,20 @@ def average(data, key=itemgetter(0), value=itemgetter(1)):
        record, return the relevant value.
     """
     for k, group in groupby(data, key):
-        yield (k, sum(value(row) for row in group))
+        yield (k, sum(value(row) for row in group) / count(group)
 
 if __name__ == "__main__":
-  reader = csv.reader(open( sys.argv[1], 'r' ), delimiter="\t")
-  headerrow = reader.next()
+    reader = csv.reader(open( sys.argv[1], 'rb' ))
+  #headerrow = reader.next()
   #survey_id, choice_id, question_name, order, preceptor_id, site_name
 
   #chart = GroupedVerticalBarChart(settings.width, settings.height,
   #    y_range=(
-  for question, avg in average(reader, key=itemgetter(2), value=lambda r:
-      if len(r[1]) > 0:
-        int(r[1])):
-    print "%10s: %d" % (question, avg)
+
+    rows = []
+    for r in reader:
+        rows.append((r[2], float(r[1])))
+
+    for question, avg in average(rows):
+        print "%10s: %d" % (question, avg)
 
